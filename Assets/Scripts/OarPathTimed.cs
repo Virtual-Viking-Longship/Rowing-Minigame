cbd47b58b41a92129fa2b2888f5e4993aa694740
Lily Haas <haasl@carleton.edu>
2023-12-21 16:02:25 -0600

    changed some instruction UI content and added lots of comments

--- Assets/Scripts/OarPathTimed.cs
+++ Assets/Scripts/OarPathTimed.cs
@@ -8,9 +8,11 @@ public class OarPathTimed : MonoBehaviour
     // helps the oar path locate itself (to disable the game object) and the oars
     public GameObject oarPath;
     public GameObject[] phantomOars;
+    // TO DO: make it so that you just have to assign the gameobjects in the editor
+    // and then in start it goes through and gets these components
     public PhantomOarTimed[] phantomOarComponents;
 
-    // score variable
+    // score variable- can this be private? does it matter?
     int score = 0;
 
     // ui elements to control
@@ -32,6 +34,8 @@ public class OarPathTimed : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // this ui activation seems redundant but leaving for now until there is time to make sure 
+        // removing it doesnt break anything
         learnForward.SetActive(true);
         stroke = time / 3;
         recovery = 2 * time / 3;
@@ -48,6 +52,7 @@ public class OarPathTimed : MonoBehaviour
 
     void OnEnable()
     {
+        // Co-routines let you put in delays in a few ways, in this case, by number of seconds
         StartCoroutine(Sequence());
     }
 
@@ -58,13 +63,21 @@ public class OarPathTimed : MonoBehaviour
 
     IEnumerator Sequence()
     {
+        // tells the WaitForSeconds how long to pause, different for oars in stroke vs recovery
         float wait;
+        // activate the UI for the first set of instructions
         learnForward.SetActive(true);
+        // wait so the user can read- eventually there will also be audio for them to listen to-
+        // may have to adjust the timing based on audio and user testing
         yield return new WaitForSeconds(5);
+        // first stage has 3 rounds
         for (int i = 0; i < 3; i++)
         {
+            // goes through the array of oars
+            // if the stroke and recovery ever end up uneven this will need to be split into 2 loops
             foreach(PhantomOarTimed oar in phantomOarComponents)
             {
+                // figure out of this target is part of stroke or recovery and assign delay accordingly
                 if (oar.stroke)
                 {
                     wait = perStroke;
@@ -73,21 +86,31 @@ public class OarPathTimed : MonoBehaviour
                 {
                     wait = perRecovery;
                 }
+                // enables the target and does some other bookkeeping
                 oar.Appear();
+                // delays the appearance of the next oar
                 yield return new WaitForSeconds(wait);
             }
+            // a break before disabling the targets so the user can try to catch up
             yield return new WaitForSeconds(3);
+            // disable all of the targets at once
             foreach(PhantomOarTimed oar in phantomOarComponents)
             {
+                // disables the target and does some other bookkeeping
                 oar.Hit();
             }
+            // quick break between rounds
             yield return new WaitForSeconds(1);
         }
+        // reset the score and enable the proper UI elements for stage 2
         ResetScore();
         learnForward.SetActive(false);
         forwardTime.SetActive(true);
         directionScore.SetActive(true);
+        // again, a break for reading/listening, time may need adjusting
         yield return new WaitForSeconds(5);
+
+        // second stage has 3 rounds
         for (int i = 0; i < 3; i++)
         {
             foreach(PhantomOarTimed oar in phantomOarComponents)
@@ -102,14 +125,20 @@ public class OarPathTimed : MonoBehaviour
                 }
                 oar.Appear();
                 yield return new WaitForSeconds(wait);
+                // now the oars will disappear as the next one appears- so timing matters
                 oar.Hit();
             }
         }
+        // break so the user can see their score
         yield return new WaitForSeconds(5);
+        // reset the score and enable the UI elements for stage 3
         ResetScore();
         forwardTime.SetActive(false);
         gameText.SetActive(true);
+        // again, a break for reading/listening, time may need adjusting
         yield return new WaitForSeconds(5);
+
+        // stage 3 is the challenge stage, currently 10 rounds, refine with testing
         for (int i = 0; i < 10; i++)
         {
             foreach(PhantomOarTimed oar in phantomOarComponents)
@@ -127,11 +156,12 @@ public class OarPathTimed : MonoBehaviour
                 oar.Hit();
             }
         }
+        // final UI changes to congratulate user and display their score
         gameText.SetActive(false);
         end.SetActive(true);
     }
 
-    // the script on the oars uses this method to add points
+    // the script on the targets uses this method to add points
     public void AddScore(int points)
     {
         score += points;
--- Assets/Scripts/PhantomOarTimed.cs
+++ Assets/Scripts/PhantomOarTimed.cs
@@ -5,11 +5,18 @@ using TMPro;
 
 public class PhantomOarTimed : MonoBehaviour
 {
+    // want to rename this at some point when there is more time to troubleshoot the inevitable
+    // missed reference to change
     public GameObject space;
     public Collider grabOarCollider;
+    // might be able to make this private and initialize it in Start or OnEnable
     public PhantomOarTimed nextOar;
+    // probably should be private
     public bool prevOarActive;
+    // does not appear to be referenced anywhere anymore, will try removing when there is time to fix
+    // things in case it breaks something
     public bool firstOar;
+    // these should be assigned in the editor still
     public OarPathTimed oarPath;
     public bool stroke;
 
@@ -27,6 +34,8 @@ public class PhantomOarTimed : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+        // make sure this is the oar hitting it
+        // make sure it is being hit in the right order (after the one before it)
         if (other == grabOarCollider && !prevOarActive) 
         {
             Hit();
